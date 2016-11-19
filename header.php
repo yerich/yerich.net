26d5237caac771badac007115e8b9da45a0bd112
Richard Ye <richard.ye@riotgames.com>
2016-11-19 01:08:39 -0800

    no longer a student

--- header.php
+++ header.php
@@ -2,14 +2,14 @@
 <html>
 <head>
     <title><?= $_PAGETITLE ? ($_PAGETITLE . " - ") : "" ?>Richard Ye</title>
-    
+
     <link rel="stylesheet" type="text/css" href="/style.css" />
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js"></script>
     <script type="text/javascript" src="/main.js"></script>
-    
+
     <meta name="viewport" content="width=device, initial-scale=1">
 </head>
 
@@ -41,9 +41,9 @@
                 </div>
             </div>
         </div>
-        
+
         <div id="tagline">
-            Developer, gaming enthusiast and CS student at the University of Waterloo
+            PC gaming enthusiast doing software development at Riot Games. Words are my own.
         </div>
     </header>
 
